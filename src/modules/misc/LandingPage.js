c995e91fae74def9d3b0b82166ccb0ea2c01ff7d
david <david@chekhub.com>
2022-07-07 21:40:15 -0400

    Cleanup

--- src/modules/misc/LandingPage.js
+++ src/modules/misc/LandingPage.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Container from '@mui/material/Container';
 export default function LandingPage() {
   return (
-    <div>
+    <Container>
       Landing Page to be built when I get access too the other repos.
-    </div>
+    </Container>
   )
 }
